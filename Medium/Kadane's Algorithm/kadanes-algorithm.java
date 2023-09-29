6932b742b8e78fb2c12d8abe4ae7f3b30ae87c7f
Nishu Lodhi <119037232+nishulodhi174@users.noreply.github.com>
2023-09-29 23:29:49 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.io.*;
+
+import java.util.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling maxSubarraySum() function
+		    System.out.println(obj.maxSubarraySum(arr, n));
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+class Solution{
+     long maxSubarraySum(int arr[], int n){
+        long maxSum=arr[0];
+        long curSum = 0;
+        for(int i=0;i<n;i++){
+            curSum = curSum + arr[i];
+            if(curSum> maxSum){
+                maxSum = curSum;
+            }
+            if(curSum<0){
+                curSum =0;
+            }
+        }
+        
+        return maxSum;
+        
+    }
+    
+}
+
